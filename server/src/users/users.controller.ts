4f46248a853f3fd348cbb82bb80984fe11e450c9
dmitry-kornienko <dima.korn1996@gmail.com>
2024-10-10 09:24:49 +0400

    feat(auth): added registration logic

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,14 +1,11 @@
 import {
 	Body,
 	Controller,
-	Delete,
-	Get,
-	Param,
-	Patch,
 	Post,
+	UsePipes,
+	ValidationPipe,
 } from '@nestjs/common'
 import { CreateUserDto } from './dto/create-user.dto'
-import { UpdateUserDto } from './dto/update-user.dto'
 import { UsersService } from './users.service'
 
 @Controller('users')
@@ -16,27 +13,13 @@ export class UsersController {
 	constructor(private readonly usersService: UsersService) {}
 
 	@Post()
+	@UsePipes(new ValidationPipe())
 	create(@Body() createUserDto: CreateUserDto) {
 		return this.usersService.create(createUserDto)
 	}
 
-	@Get()
-	findAll() {
-		return this.usersService.findAll()
-	}
-
-	@Get(':id')
-	findOne(@Param('id') id: string) {
-		return this.usersService.findOne(+id)
-	}
-
-	@Patch(':id')
-	update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-		return this.usersService.update(+id, updateUserDto)
-	}
-
-	@Delete(':id')
-	remove(@Param('id') id: string) {
-		return this.usersService.remove(+id)
-	}
+	// @Get()
+	// findOne() {
+	// 	return this.usersService.findOne()
+	// }
 }
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,26 +1,39 @@
-import { Injectable } from '@nestjs/common'
+import { BadRequestException, Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import * as bcrypt from 'bcrypt'
+import { randomUUID } from 'crypto'
+import { Repository } from 'typeorm'
 import { CreateUserDto } from './dto/create-user.dto'
-import { UpdateUserDto } from './dto/update-user.dto'
+import { User } from './entities/user.entity'
 
 @Injectable()
 export class UsersService {
-	create(createUserDto: CreateUserDto) {
-		return 'This action adds a new user'
+	constructor(
+		@InjectRepository(User) private readonly userRepository: Repository<User>
+	) {}
+
+	async create(createUserDto: CreateUserDto) {
+		const existUser = await this.userRepository.findOne({
+			where: {
+				email: createUserDto.email,
+			},
+		})
+
+		if (existUser) throw new BadRequestException('This email already exist')
+
+		const hashedPassword = await bcrypt.hash(createUserDto.password, 10)
+		const activationLink = randomUUID()
+
+		const user = await this.userRepository.save({
+			email: createUserDto.email,
+			password: hashedPassword,
+			activationLink,
+		})
+
+		return { user }
 	}
 
-	findAll() {
-		return `This action returns all users`
-	}
-
-	findOne(id: number) {
-		return `This action returns a #${id} user`
-	}
-
-	update(id: number, updateUserDto: UpdateUserDto) {
-		return `This action updates a #${id} user`
-	}
-
-	remove(id: number) {
-		return `This action removes a #${id} user`
-	}
+	// findOne(id: number) {
+	// 	return `This action returns a #${id} user`
+	// }
 }
